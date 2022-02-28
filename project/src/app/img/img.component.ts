b4b845c708900ef91044280eda54d4a593085f02
Najmi Mehdi <najmimehdi70@gmail.com>
2022-02-28 20:00:58 +0100

    edited by Mehdi

--- project/src/app/img/img.component.ts
+++ project/src/app/img/img.component.ts
@@ -6,13 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./img.component.css']
 })
 export class ImgComponent  {
-  
+
   ImagePath: string;
   first_string = 'Welcome';
-  
+
 
   constructor(){
-    this.ImagePath = '/assets/4233.jpg';
+    this.ImagePath = '/assets/photo.jpeg';
   }
 
 }
