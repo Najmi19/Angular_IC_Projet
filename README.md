7f08cda261c4137c37094d52c03ea8246933e114
EdMkn <nmanyim@rocketmail.com>
2022-03-03 08:39:23 +0100

    reorganisation-projet

---
+++ README.md
@@ -0,0 +1,6 @@
+# angular-java-example
+This is an example project of angular with java
+Maven 3.6.3 compatible avec opnjdk 8
+https://stackoverflow.com/questions/44438848/maven-crashes-when-trying-to-compile-a-project-error-executing-maven
+https://stackoverflow.com/questions/68182090/cant-install-correct-angular-cli-version
+https://medium.com/bb-tutorials-and-thoughts/how-to-develop-and-build-angular-app-with-java-backend-87fb603c6e17
---
+++ src/main/ui/src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
+import { ImgComponent } from './img/img.component';
+import { Routes, RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import { LogoComponent } from './logo/logo.component';
+import { CarComponent } from './car/car.component';
+import { MotoComponent } from './moto/moto.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FirstComponent,
+    ImgComponent,
+    LogoComponent,
+    CarComponent,
+    MotoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+
+
+  ],
+
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
